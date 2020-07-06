1ad8bc8634574a709899990738568508eaa635b6
sheready <csstlw@gmail.com>
2020-07-06 14:36:32 +0300

    add headlines route and pass several articles

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def index():
     
     mylist = zip(news, desc, img, date, url)
 
-    return render_template('aljazeera.html', context = mylist)
+    return render_template('mashable.html', context = mylist)
 
-@app.route('/bbc')
+@app.route('/techcrunch')
 def bbc():
     newsapi = NewsApiClient(api_key = 'b3fefd9be43745ce806ac142643aca1e')
     topheadlines = newsapi.get_top_headlines(sources = 'techcrunch')
@@ -60,9 +60,9 @@ def bbc():
     
     mylist = zip(news, desc, img, date, url)
 
-    return render_template('bbc.html', context = mylist)
+    return render_template('techcrunch.html', context = mylist)
 
-@app.route('/abc')
+@app.route('/wired')
 def abc():
     newsapi = NewsApiClient(api_key = 'b3fefd9be43745ce806ac142643aca1e')
     topheadlines = newsapi.get_top_headlines(sources = 'wired')
@@ -82,11 +82,37 @@ def abc():
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
         date.append(myarticles['publishedAt'])
-        date.append(myarticles['url'])
+        url.append(myarticles['url'])
     
     mylist = zip(news, desc, img, date, url)
 
-    return render_template('abc.html', context = mylist)
+    return render_template('wired.html', context = mylist)
+
+@app.route('/headlines')
+def head():
+    newsapi = NewsApiClient(api_key = 'b3fefd9be43745ce806ac142643aca1e')
+    topheadlines = newsapi.get_top_headlines(sources = 'cbs-news, cnn, the-wall-street-journal')
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+    date = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        date.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+    
+    mylist = zip(news, desc, img, date, url)
+
+    return render_template('index.html', context = mylist)
 
 if __name__ == "__main__":
     app.run(debug = True)
